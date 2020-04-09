42d16d6c90c5b64cdbf599ef6e1aa1e0bc2077fc
dante <dante@marketdesiners.com>
2020-04-09 18:28:46 +0900

    일단 한대까지는 해놓고 보자

--- api/v1/auth.py
+++ api/v1/auth.py
@@ -1,5 +1,6 @@
 from datetime import datetime
 from flask import Blueprint, request, redirect, url_for, jsonify
+
 from marshmallow import Schema, fields, pprint
 
 import wtforms_json
@@ -14,20 +15,24 @@ class UserSchema(Schema):
     name = fields.Str()
     last_login_at = fields.Date()
 
+    class Meta:
+        fields = ("name", "email", "last_login_at")
+        ordered = True
+
+
 
 wtforms_json.init()
 api_auth_v1 = Blueprint('api_auth_v1', __name__)
 
 @api_auth_v1.route('/login', methods=['POST'])
 def login():
-    form = LoginForm.from_json(request.json)
-    print(form.validate())
-    print(form.data)
-    print(request.json)
+
+    form = LoginForm.from_json(request.json, csrf_enabled=False)
 
     if form.validate():
-        return jsonify({'status': 200})
-    else:
-        return jsonify({'status': 400})
+        user = User.where('email', form.email.data).first()
+        if user is not None and check_password_hash(user.password, form.password.data):
+            user.update(last_login_at=datetime.now())
+            return jsonify({'status': 200, 'data': UserSchema().dump(user)})
 
-    return ''
+    return jsonify({'status': 400})
